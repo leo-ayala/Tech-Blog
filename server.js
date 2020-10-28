10699384b8a45126284d65b6baf6468af506970e
Leo Ayala <leo_a1016@yahoo.com>
2020-10-28 19:26:39 -0400

    fixing heroku

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const path = require('path');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
-const PORT = process.env.PORT 
-// || 3001;
+const PORT = process.env.PORT || 3001;
 
 const sess = {
   secret: 'Super secret secret',
