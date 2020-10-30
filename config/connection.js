fe9c2876c597fbf516fb478090d5507421b25ceb
Leo Ayala <leo_a1016@yahoo.com>
2020-10-30 15:42:26 -0400

    fixed typo

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 let sequelize;
 require('dotenv').config();
 
-if (procces.env.JAWSDB_URL) {
+if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL );
 } else {
  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
